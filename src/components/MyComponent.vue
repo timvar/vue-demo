458fbc2a2c1068da7b6e1710fd6b879aaf9b0727
Timo Varila <timo.varila@myy.haaga-helia.fi>
2019-02-08 13:25:41 +0200

    MyComponent

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="mycomponent">
+    <h1>{{ myMsg }}</h1>
+    
+    
+   
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyComponent',
+  props: {
+    myMsg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- Add components here -- <MyComponent props="" /> -->
+    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import MyComponent from '@/components/MyComponent.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    //MyComponent
   }
 }
 </script>
